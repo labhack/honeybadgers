ab172a164bb0be53c81d321ac6d78790b2f98d1c
James Fourman <zerocooldoh@gmail.com>
2014-10-25 15:05:28 -0400

    Adding rermote sql settings

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -57,8 +57,11 @@ WSGI_APPLICATION = 'hackathon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hackathon',
+        'USER': 'hackathon',
+        'PASSWORD': 'hackathon',
+        'HOST': '104.131.124.17',
     }
 }
 
