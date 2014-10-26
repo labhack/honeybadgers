01f1bfb5b9a9bbc5d8b5738f625ae5bf6866eb78
Matthew Jacobsen <jacobsen.6@gmail.com>
2014-10-25 22:35:42 -0400

    Submitting data for views, templates, etc. - general interface capabilities.

--- hackathon/dataviewer/views.py
+++ hackathon/dataviewer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+def index(request):
+	testtext = 'dataviewer text'
+	template = loader.get_template('dataviewer/index.html')
+	context = RequestContext(request, {
+		'testext': testtext,
+	})
+	return HttpResponse(template.render(context))
 # Create your views here.
--- hackathon/hackathon/urls.py
+++ hackathon/hackathon/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hackathon.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^dataviewer/', include('dataviewer.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
